33dd2936a8f144ddb3a719fca6f508fccf163527
d-taylor6403 <dannielletaylor@outlook.com>
2020-12-21 18:13:59 -0600

    initial game setup

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+//window.alert("this is an alert! JavaScript is running!")
+
+//window prompt to obtain Robot name
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+//This creates a function name "fight"
+
+function fight() {
+    window.alert("The fight has begun!" + playerName);
+}
+
+//fight();
