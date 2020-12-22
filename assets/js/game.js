5e481afbf12d73101dd373745687d0e7868bd5ff
d-taylor6403 <dannielletaylor@outlook.com>
2020-12-21 18:51:00 -0600

    lesson save

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,22 @@
 //window prompt to obtain Robot name
 
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+
+//Console log all three variables to check the status
+console.log(playerName, playerHealth, playerAttack);
+
+//PC Robot
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 //This creates a function name "fight"
 
-function fight() {
-    window.alert("The fight has begun!" + playerName);
-}
+var fight = function() {
+    window.alert("Welcome to Gladiator's Revenge!")
+};
 
-//fight();
+fight();
