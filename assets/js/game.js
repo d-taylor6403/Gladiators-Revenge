4c2c5133b36585f90a0dc1569a3f3b838104fce7
d-taylor6403 <dannielletaylor@outlook.com>
2020-12-29 22:16:16 -0600

    random health and attack values added

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,7 +31,7 @@ console.log(playerName, playerHealth, playerAttack);
 //PC Robot
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
-var enemyAttack = 12;
+var enemyAttack = 10;
 
 
 //This creates a function name "fight"
@@ -50,16 +50,17 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert(playerName + ' has decided to skip this fight. Goodbye!');
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney", playerMoney)
                 break;
             }
         }
 
-        // remove enemy's health by subtracting the amount set in the playerAttack variable
-        enemyHealth = enemyHealth - playerAttack;
+        // generate random damage value based on player's attack power
+        var damage = randomNumber(playerAttack - 5, playerAttack)
+        enemyHealth = Math.max(0, enemyHealth - damage);
         console.log(
-            playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
+            playerName + ' attacked ' + enemyName + ' with a damage of ' + damage + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
         );
 
         // check enemy's health
@@ -77,9 +78,10 @@ var fight = function(enemyName) {
         }
 
         // remove players's health by subtracting the amount set in the enemyAttack variable
-        playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 5, enemyAttack)
+        playerHealth = Math.max(0, playerHealth - damage);
         console.log(
-            enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
+            enemyName + ' attacked ' + playerName + ' with a damage of ' + damage + ". " + playerName + ' now has ' + playerHealth + ' health remaining.'
         );
 
         // check player's health
@@ -110,7 +112,7 @@ var startGame = function() {
             var pickedEnemyName = enemyNames[i];
 
             //reset enemyHealth before starting new fight
-            enemyHealth = 50;
+            enemyHealth = randomNumber(10, 100);
 
             //debugger to pause script 
             //debugger;
@@ -205,6 +207,13 @@ var shop = function() {
             shop();
             break;
     }
+};
+
+//function to generate random value
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 };
 
 //Start the game when the page loads
